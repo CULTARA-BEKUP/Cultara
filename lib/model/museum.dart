a9adfe0a37070a949ad00cfa7789271ddfb48497
rahmadnoorikhsan <rajaikhsan2021@gmail.com>
2025-10-21 21:34:58 +0700

    Create Feature Museum

---
+++ lib/model/museum.dart
@@ -0,0 +1,14 @@
+
+class Museum {
+  final String id;
+  final String name;
+  final String description;
+  final String image;
+
+  Museum({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.image,
+  });
+}
